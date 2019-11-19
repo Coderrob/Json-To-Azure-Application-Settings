a99d902627d4a3ffe341df5c89adb8202c648a49
Coderrob <Rob@Coderrob.com>
2019-11-19 11:15:07 -0600

    Add quick json export test.

---
+++ src/FileFormatting.Tests/JsonFormattingTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace FileFormatting.Tests
+{
+    public class JsonFormattingTests
+    {
+        private readonly JsonFormatting _formatting;
+
+        public JsonFormattingTests()
+        {
+            var logger = new Mock<ILogger<JsonFormatting>>();
+            _formatting = new JsonFormatting(logger.Object);
+        }
+
+        //[Fact]
+        public async Task LoadJsonSuccessfully()
+        {
+            await _formatting
+                .Load("./TestData/sample.json")
+                .WriteToFile("c:\\devspace\\v300-json.csv");
+        }
+    }
+}
